bb34c7fee8d8d17df2fb8e878e0ab4dcf2d15298
reza477 <alexfitzgrld@gmail.com>
2019-09-03 01:26:29 +0000

    post

--- express_server.js
+++ express_server.js
@@ -1,7 +1,8 @@
 const express = require("express");
 const app = express();
 const PORT = 8080; // default port 8080
-
+const bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({extended: true}));
 
 
 app.set('view engine', 'ejs');
@@ -12,15 +13,28 @@ const urlDatabase = {
   "9sm5xK": "http://www.google.com"
 };
 
+
+
 app.get("/", (req, res) => {
   res.send("Hello!");
 });
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
 });
+
 app.get("/hello", (req, res) => {
   res.send("<html><body>Hello <b>World</b></body></html>\n");
 });
+app.get("/urls/new", (req, res) => {
+  res.render("urls_new");
+});
+
+
+app.get("/urls/:shortURL",(req, res) => {
+  let templateVars = {shortURL: req.params.shortURL, longURL: urlDatabase[req.params.shortURL]}
+  res.render("url_shows", templateVars);
+});
+
 app.get("/url", (req, res) => {
   let templateVars = { urls: urlDatabase, test :"test" };
   res.render("urls_index", templateVars);
