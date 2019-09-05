8f40f4f99a697484da2da6d9d290b6e1cecde426
reza477 <alexfitzgrld@gmail.com>
2019-09-05 03:22:08 +0000

    something something2

---
+++ views/Untitled-12.js
@@ -0,0 +1,9 @@
+GET /posts -> browse all posts
+POST /posts -> create a new post
+GET /posts/:id -> read a particular post
+POST /posts/:id -> edit a particular post
+
+GET /comments -> browse all comments
+POST /comments -> create a new comments
+GET /comments/:id -> read a particular comments
+POST /comments/:id -> edit a particular comments
